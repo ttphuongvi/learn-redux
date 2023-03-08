9645090996191d124bfa4f8d2383450470824eca
Thị Phương Vi Trịnh <trthphuongvi2901@gmail.com>
2023-03-08 09:15:28 +0700

    add tab list product

--- src/App.js
+++ src/App.js
@@ -1,27 +1,86 @@
-import { useDispatch, useSelector } from 'react-redux';
-import './App.css';
-import { decrease, increase } from './store/actions/counerAction';
+import "./App.css";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import { Box } from "@mui/material";
+import React from "react";
+import PropTypes from "prop-types";
+import Home from "./page/Home";
+import ListProducts from "./page/ListProducts";
 
-function App() {
-  const {counter} = useSelector((state) => state.counter); // lấy biến counter từ store 
-
-  const dispatch = useDispatch(); // khởi tạo dispatch
-
-  const handleDecrease = () => {
-    dispatch(decrease()); 
-  }; // gửi hành động giảm
-
-  const handleIncrease = () => {
-    dispatch(increase()); 
-  }; // gửi hành động tăng
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
   return (
-    <div className="App">
-     <h2>Learn to Redux</h2>
-     <div>
-      <button onClick={handleDecrease}>-</button> {counter} <button onClick={handleIncrease}>+</button>
-     </div>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
     </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+function App() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const tabs = [
+    {
+      label: "Trang chủ",
+      content: <Home />,
+    },
+    {
+      label: "Sản phẩm",
+      content: <ListProducts />,
+    },
+  ];
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          {tabs.map((tab, index) => {
+            return (
+              <Tab
+                label={tab.label}
+                {...a11yProps({ index })}
+                key={tab.label}
+              />
+            );
+          })}
+        </Tabs>
+      </Box>
+      {tabs.map((tab, index) => {
+        return (
+          <TabPanel value={value} index={index} key={tab.label}>
+            {tab.content}
+          </TabPanel>
+        );
+      })}
+    </Box>
   );
 }
 
